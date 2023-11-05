ea90f0b94a485e3582333533082a2f5b7fde3a0a
yxu <penguinxx2@gmail.com>
2023-11-05 20:36:22 +0900

    update

--- cheak_map.c
+++ cheak_map.c
@@ -6,7 +6,49 @@
 /*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 17:04:34 by yxu               #+#    #+#             */
-/*   Updated: 2023/10/28 17:04:35 by yxu              ###   ########.fr       */
+/*   Updated: 2023/11/05 19:55:37 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+
+int	count_line(void)
+{
+	int		fd;
+	int		nb;
+	char	*line;
+
+	fd = open(MAP_PATH, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	nb = 0;
+	line = read_next_line(fd);
+	while (line)
+	{
+		nb++;
+		free(line);
+		line = read_next_line(fd);
+	}
+	close(fd);
+	return (nb);
+}
+
+char	**read_map(int fd, int rows)
+{
+	char	**map;
+	int		i;
+
+	map = (char **)malloc(sizeof(char *) * (rows + 1));
+	map[rows] = NULL;
+	i = 0;
+	while (i < rows)
+		map[i++] = read_next_line(fd);
+	return (map);
+}
+
+int	check_map(char **map)
+{
+
+
+	
+}
---
+++ so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 18:21:34 by yxu               #+#    #+#             */
+/*   Updated: 2023/11/05 19:32:42 by yxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# include <fcntl.h>
+
+# ifndef MAP_PATH
+#  define MAP_PATH "./maps/map.ber"
+# endif
+
+#endif
