95dd93ef7fe5ec4044c50bfc01087987e6ab8618
yxu <penguinxx2@gmail.com>
2023-12-12 18:04:29 +0900

    add llib

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 18:21:34 by yxu               #+#    #+#             */
-/*   Updated: 2023/11/05 19:32:42 by yxu              ###   ########.fr       */
+/*   Updated: 2023/12/12 17:03:15 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,19 @@
 # include <string.h>
 # include <math.h>
 # include <fcntl.h>
+# include <mlx.h>
+# include <errno.h>
 
 # ifndef MAP_PATH
 #  define MAP_PATH "./maps/map.ber"
 # endif
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
 #endif
